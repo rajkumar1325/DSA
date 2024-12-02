0405fee2a7b58cf3c5a2cf25f170c52ed672b2e7
Raj kumar <rajkumar.rk0104@gmail.com>
2024-12-03 02:58:32 +0530

    Create @Pattern-Alphabet.cpp
    Printing a Pattern of Alphabet in increasing order.

---
+++ @Pattern-Alphabet.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int number = 6; 
+    char ch = 'A'; //CREATING A STARTING VARIABLE
+
+    for (int c = 0; c <= number && ch<='Z'; c++)
+    {
+        for (int a = 0; a <= number && ch<= 'Z'; a++)
+        {
+            cout << ch << " ";
+            ch++;
+            // RESETTING THE ALPHABET
+            if (ch > 'Z')
+            {
+                ch = 'A';
+            }
+            
+        }
+        cout << endl;
+    }
+    return 0;
+}
