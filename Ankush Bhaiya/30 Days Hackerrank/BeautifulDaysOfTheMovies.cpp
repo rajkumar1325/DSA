b7ee19e757696b21d1d3c2cd9ca75c5f18c11198
Raj kumar <rajkumar.rk0104@gmail.com>
2025-02-08 01:33:34 +0530

    BeautifulDaysOfTheMovies.cpp --> Hackerrank D9 -Q4

---
+++ Ankush Bhaiya/30 Days Hackerrank/BeautifulDaysOfTheMovies.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+int reversing(int num){
+        int ans=0;
+        while (num!=0) {
+            int rem = num%10;
+            num /= 10;
+            ans = ans*10 + rem;
+        }
+    return ans;
+}
+
+
+int beautifulDays(int i, int j, int k) {
+//Beautiful numbers : |i-rev(i)|  is evenly divisible by k.
+    int beautifulDay=0;
+    int value=0;
+    
+    // for (i; i<=j; i++)  //gives error
+    for (; i<=j; i++) { //i is already defined,so no need to redefine it.
+
+        int reverse = reversing(i);
+        
+        value = abs(i-reverse); // i-reverse(i)  
+        
+        if (value%k ==0) {
+            beautifulDay++;
+        }
+    }
+    return beautifulDay;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int i = stoi(first_multiple_input[0]);
+
+    int j = stoi(first_multiple_input[1]);
+
+    int k = stoi(first_multiple_input[2]);
+
+    int result = beautifulDays(i, j, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
