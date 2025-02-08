abb896cfa07fbf1ed5db04891a109a05d4dee8f0
Raj kumar <rajkumar.rk0104@gmail.com>
2025-02-08 09:55:16 +0530

    Function Pass by value and Pass by Reference with code
    https://www.youtube.com/watch?v=PnSgN5WOUC0&list=PLQEaRBV9gAFu4ovJ41PywklqI7IyXwr01&index=19&t=2s

---
+++ Function(passValue_passReference).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// PASS BY VALUE 
+// Any changes made inside the function do not affect the original n in main().
+// Pass-by-value (int a) --> Original variable remains unchanged.
+/*
+int increment(int a){
+    a++;
+    cout<<"The inner value "<< a<<endl;
+    return a;
+}
+int main(){
+    int n=5;
+    increment(n);   //can't able to update this n
+    cout<<n;
+
+}
+*/
+
+
+//PASS BY REFERENCE.
+// Pass-by-reference (int &a) --> Original variable gets modified.
+int increment(int &a){
+    a++;
+    cout<<"The inner value "<< a<<endl;
+    return a;
+}
+int main(){
+    int n=5;
+    increment(n);   //can't able to update this n
+    cout<<n;
+
+}
