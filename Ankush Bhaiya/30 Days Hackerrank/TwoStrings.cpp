955ce74ded248530eafb47734e6cec9c0e67736e
Raj kumar <rajkumar.rk0104@gmail.com>
2025-03-02 20:13:12 +0530

    Two Strings --> day25 Q1
    https://www.hackerrank.com/challenges/two-strings/problem?isFullScreen=true

---
+++ Ankush Bhaiya/30 Days Hackerrank/TwoStrings.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'twoStrings' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+string twoStrings(string s1, string s2) {
+    unordered_map<char , int> freq1;
+    
+    // traversing string and counting occurances
+    for(char ch: s1){
+        freq1[ch]++;
+    }
+    
+    
+    bool result = false;
+    // traversing mapping and check if it contains atleast 1 key same
+    for (auto c : s2) { 
+        // check if a char c exists in an map freq1.
+        if (freq1.find(c) != freq1.end()) {
+            result = true;
+        }
+    }
+    
+    if (result) {
+        return "YES";
+    }
+    else {
+        return "NO";
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s1;
+        getline(cin, s1);
+
+        string s2;
+        getline(cin, s2);
+
+        string result = twoStrings(s1, s2);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
