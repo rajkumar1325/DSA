2f1b0b1ecd801a3017d588c22c90cbc44ac310ed
Raj kumar <rajkumar.rk0104@gmail.com>
2025-06-12 00:48:03 +0530

    Create 05_nextPermutation.cpp
    https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/next-permutation5226

---
+++ GFG 160/Array/05_nextPermutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        next_permutation(arr.begin(), arr.end());
+    }
+};
+
+
+
+// Question Link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/next-permutation5226
