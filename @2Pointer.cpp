beb91875d3e97d75a853e51ba2678a7bcabda83e
Raj kumar <rajkumar.rk0104@gmail.com>
2025-11-29 20:33:33 +0530

    Implement 2-pointer approach to find target sum

---
+++ @2Pointer.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+// ....................................2 pointer approach..............................................
+/*
+    KEY RULES TO REMEMBER -- {This ONLY works because the array is sorted.}
+    ✔ If sum < target → move left forward
+                    Because left forward means bigger numbers.
+
+    ✔ If sum > target → move right backward
+                    Because right backward means smaller numbers.
+*/
+
+int main(){
+    int target = 10;
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,23};
+    int n = arr.size();
+
+    // using 2 pointer
+    int left = 0;
+    int right = n-1;
+
+    while (arr[left]<arr[right])
+    {
+        if(arr[left] + arr[right] == target){
+            cout<<"Found and the elements are "<< arr[left]<< "and" << arr[right];
+            return 0;
+        }
+
+        else if((arr[left] + arr[right]) < target){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+    cout<<"Not Found";
+
+
+}
